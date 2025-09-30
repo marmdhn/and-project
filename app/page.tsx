77db07628feb6aae57c96cc6e61a56eba92ef22c
marmdhn <makbarramadhan62@gmail.com>
2025-09-30 14:16:37 +0700

    refactor: Integrate `ServiceSection`, `WhyUsSection`, and `CtaSection` into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import HeroSection from "@/components/sections/HeroSection";
+import ServiceSection from "@/components/sections/Service";
+import WhyUsSection from "@/components/sections/WhyUs";
+import CtaSection from "@/components/sections/Cta";
 
 export default function Home() {
   return (
     <div className="px-5 lg:px-20">
       <HeroSection />
-
-      <div className="lg:px-20"></div>
+      <div className="lg:px-20">
+        <ServiceSection />
+        <WhyUsSection />
+        <CtaSection />
+      </div>
     </div>
   );
 }
