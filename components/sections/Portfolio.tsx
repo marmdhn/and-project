a2f6e9edb88b1f490abfffa6eb4cbbd74649c13c
marmdhn <makbarramadhan62@gmail.com>
2025-09-30 14:16:02 +0700

    feat: Add `PortfolioSection` component with portfolio items and category badges

---
+++ components/sections/Portfolio.tsx
@@ -0,0 +1,66 @@
+import Button from "../Button";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+interface Portfolio {
+  title: string;
+  img: string;
+  categories: string[];
+  text: string;
+  redirect?: string;
+}
+export default function PortfolioSection() {
+  const portfolioData: Portfolio[] = [
+    {
+      title: "Portfolio 1",
+      img: "/portfolio/portfolio_1.png",
+      categories: ["UI/UX Design", "Website"],
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      redirect: "/kataKopi",
+    },
+    {
+      title: "Portfolio 2",
+      img: "/portfolio/portfolio_1.png",
+      categories: ["UI/UX Design", "Website"],
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      redirect: "/kataKopi",
+    },
+  ];
+  return (
+    <section id="portfolio section" className="section">
+      <div className="flex items-center justify-between">
+        <h1 className="text-title text-3xl">🎨 Our Portfolio</h1>
+        <Button text="View All" icon={true} />
+      </div>
+
+      <div className="px-10">
+        {portfolioData.map((item, index) => (
+          <div
+            key={index}
+            className={`flex ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"} justify-between items-center w-full`}
+          >
+            <img src={item.img} alt="portfolio image" className="w-1/2" />
+            <div
+              className={`flex flex-col justify-center ${index % 2 === 0 ? "items-end" : "items-start"} gap-2 w-1/2`}
+            >
+              <h1 className="text-title text-3xl">{item.title}</h1>
+              <div className="flex gap-2">
+                {item.categories.map((item, index) => (
+                  <span key={index} className="badge badge-primary">
+                    {item}
+                  </span>
+                ))}
+              </div>
+              <h5 className="text-lg text-gray">{item.text}</h5>
+              {item.redirect && (
+                <Link to={item.redirect}>
+                  <FaExternalLinkAlt className="text-gray cursor-pointer hover:text-primary" />
+                </Link>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
