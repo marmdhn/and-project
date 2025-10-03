11bc929eae1b7da29865e3f4ba91c7f50276a752
marmdhn <makbarramadhan62@gmail.com>
2025-10-03 21:24:15 +0700

    refactor: Improve `ProductCard` layout, enhance badge styles, and optimize spacing

--- components/ProducCard.tsx
+++ components/ProducCard.tsx
@@ -9,8 +9,12 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <div className="flex justify-between items-center m-2">
-        {product.isNew && <div className="badge badge-secondary">NEW</div>}
-        <div className="badge badge-outline">{product.theme}</div>
+        <div className="badge badge-outline text-gray">{product.theme}</div>
+        {product.isNew && (
+          <div className="badge badge-secondary font-bold animate-pulse">
+            NEW
+          </div>
+        )}
       </div>
       <figure className="p-2 hover-gallery">
         {product.image.map((img, index) => (
@@ -25,24 +29,22 @@ export default function ProductCard({ product }: ProductCardProps) {
         ))}
       </figure>
       <div className="card-body">
-        <div className="flex justify-between items-center">
-          <h2 className="card-title">
-            {product.productTemplate} | {product.productPackage}
-          </h2>
-          <div>
-            <span
-              className={`text-sm ${product.discount && "line-through"} text-error`}
-            >
-              Rp {product.price.toLocaleString()}
+        <h2 className="card-title">
+          {product.productTemplate} | {product.productPackage}
+        </h2>
+        <div>
+          <span
+            className={`text-sm ${product.discount && "line-through"} text-error`}
+          >
+            Rp {product.price.toLocaleString()}
+          </span>
+          {product.discount && (
+            <span className="ms-2 text-lg font-semibold text-gray">
+              Rp {(product.price - product.discount).toLocaleString()}
             </span>
-            {product.discount && (
-              <span className="ms-2 text-lg font-semibold text-gray">
-                Rp {(product.price - product.discount).toLocaleString()}
-              </span>
-            )}
-          </div>
+          )}
         </div>
-        <div className="card-actions justify-center items-center">
+        <div className="card-actions justify-between items-center">
           <button className="px-3 py-1 bg-primary text-white rounded-xl transition-colors hover:bg-primary/80">
             Preview
           </button>
