d4c867eee64c97de3aaab334fe0b720f903ea898
marmdhn <makbarramadhan62@gmail.com>
2025-10-03 16:59:12 +0700

    feat: Add `productList` module with `ProductCard` interface and sample data

---
+++ data/productList.ts
@@ -0,0 +1,30 @@
+export interface ProductCard {
+  productId: number;
+  isNew: boolean;
+  theme: string;
+  image: string[];
+  productTemplate: string;
+  productPackage: string;
+  price: number;
+  discount?: number;
+  portfolio: boolean;
+}
+
+export const productList: ProductCard[] = [
+  {
+    productId: 1,
+    isNew: true,
+    theme: "Elegant & Minimaslist",
+    image: [
+      "/products/invitation/Premium A - EM 1.png",
+      "/products/invitation/Premium B - EM 1.png",
+      "/products/invitation/Standart A - EM 1.png",
+      "/products/invitation/Standart B - EM 1.png",
+    ],
+    productTemplate: "EM 1",
+    productPackage: "Standart A",
+    price: 100000,
+    discount: 5000,
+    portfolio: false,
+  },
+];
