c82b60a313c7fa1920f74a4f0fd58f420cc4f279
marmdhn <makbarramadhan62@gmail.com>
2025-10-03 16:59:23 +0700

    refactor: Add `style` and `disabled` props to `Button` component for enhanced customization options

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,6 +8,8 @@ interface ButtonProps {
   forceText?: boolean;
   type?: "button" | "link";
   href?: string;
+  style?: "button" | "underline";
+  disabled?: boolean;
 }
 
 export default function Button({
@@ -17,15 +19,21 @@ export default function Button({
   forceText = false,
   type = "button",
   href,
+  style = "button",
+  disabled = false,
 }: ButtonProps) {
   const buttonEl = (
     <div
-      onClick={type === "button" ? onClick : undefined}
-      className="bg-secondary p-1 rounded-full flex items-center gap-1 button-hover"
+      onClick={type === "button" && !disabled ? onClick : undefined}
+      className={`${
+        style === "button"
+          ? "bg-secondary p-1 rounded-full flex items-center gap-1 button-hover"
+          : "btn-underline"
+      }${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
     >
       <button
         type="button"
-        className={`btn btn-primary font-medium !rounded-full border-0 will-change-transform flex-1 ${forceText ? "inline" : "hidden sm:inline"}`}
+        className={`${style === "button" ? "btn btn-primary font-medium !rounded-full border-0 will-change-transform flex-1" : ""} ${forceText ? "inline" : "hidden sm:inline"} ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
       >
         {text}
       </button>
@@ -35,7 +43,12 @@ export default function Button({
 
   if (type === "link" && href) {
     return (
-      <Link href={href} className="inline-block">
+      <Link
+        href={href}
+        className={`inline-block ${
+          disabled ? "pointer-events-none opacity-50" : "cursor-pointer"
+        }`}
+      >
         {buttonEl}
       </Link>
     );
