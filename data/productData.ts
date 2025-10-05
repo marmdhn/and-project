b94be0816aac1ecb50a9f479760a0eae170c355c
marmdhn <makbarramadhan62@gmail.com>
2025-10-05 21:04:19 +0700

    refactor: Rename `productList` to `productData` and add `productCategoryWithFilters` for enhanced filtering options

--- data/productData.ts
+++ data/productData.ts
@@ -1,3 +1,49 @@
+export interface IProductFilter {
+  category: string;
+  filter?: {
+    binding: keyof IProductList;
+    data: string[];
+  };
+}
+
+export const productCategoryWithFilters: IProductFilter[] = [
+  {
+    category: "All",
+  },
+  {
+    category: "Catalog Website",
+  },
+  {
+    category: "Portfolio Website",
+  },
+  {
+    category: "Invitation Website",
+    filter: {
+      binding: "theme",
+      data: [
+        "Elegant & Minimalist",
+        "Floral & Pastel",
+        "Glam & luxury",
+        "Nature / Rustic",
+        "Illustrasi / Kartun",
+        "Tradisional / Cultural",
+      ],
+    },
+  },
+  {
+    category: "Branding Kit Design",
+  },
+  {
+    category: "Presentation Slide Design",
+  },
+  {
+    category: "Invitation Design Design",
+  },
+  {
+    category: "Catalog Design Design",
+  },
+];
+
 export interface IProductList {
   productId: number;
   isNew: boolean;
@@ -11,11 +57,11 @@ export interface IProductList {
   portfolio: boolean;
 }
 
-export const productList: IProductList[] = [
+export const productData: IProductList[] = [
   {
     productId: 1,
     isNew: true,
-    theme: "Elegant & Minimaslist",
+    theme: "Elegant & Minimalist",
     image: [
       "/products/invitation/Premium A - EM 1.png",
       "/products/invitation/Premium B - EM 1.png",
@@ -32,7 +78,7 @@ export const productList: IProductList[] = [
   {
     productId: 2,
     isNew: false,
-    theme: "Elegant & Minimaslist",
+    theme: "Elegant & Minimalist",
     image: [
       "/products/invitation/Premium A - EM 1.png",
       "/products/invitation/Premium B - EM 1.png",
@@ -49,7 +95,24 @@ export const productList: IProductList[] = [
   {
     productId: 3,
     isNew: false,
-    theme: "Elegant & Minimaslist",
+    theme: "Elegant & Minimalist",
+    image: [
+      "/products/invitation/Premium A - EM 1.png",
+      "/products/invitation/Premium B - EM 1.png",
+      "/products/invitation/Standart A - EM 1.png",
+      "/products/invitation/Standart B - EM 1.png",
+    ],
+    productTemplate: "EM 1",
+    productPackage: "Standart A",
+    price: 100000,
+    discount: 5000,
+    category: "invitation website",
+    portfolio: false,
+  },
+  {
+    productId: 4,
+    isNew: false,
+    theme: "Elegant & Minimalist",
     image: [
       "/products/invitation/Premium A - EM 1.png",
       "/products/invitation/Premium B - EM 1.png",
